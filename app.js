9f2fa689bb4081572b626249afc370925cd4f966
dar768 <153826948+dar768@users.noreply.github.com>
2023-12-16 22:39:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 let express = require("express");
 let app = express();
-//let PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT || 3000;
 let path = require("path");
 let mongoose = require("mongoose");
 
 let methodOverride = require("method-override");
 let postRoutes = require("./routes/blogRoutes");
 
-require("dotenv").config();
-
-let db = process.env.MONGO_URL;
-let PORT = process.env.PORT;
-
-//let db = "mongodb+srv://daria:dgHb.RZc2isKf2b@cluster0.znxpfdb.mongodb.net/Node-blog";
+let db = "mongodb+srv://daria:dgHb.RZc2isKf2b@cluster0.znxpfdb.mongodb.net/Node-blog";
 
 app.set('view engine', 'ejs');
 let createPath = (page) => path.join(__dirname, "views", `${page}.ejs`);
